52349ecc5fe99ae860e788e626a2e0d236276d80
InnerPeace <dk.optimism.io@gmail.com>
2023-02-09 03:42:52 +0200

    CI/CD

--- internal/repository/db.go
+++ internal/repository/db.go
@@ -49,7 +49,8 @@ func NewDB() (*sql.DB, error) {
 }
 
 func NewTestDB() (*sql.DB, error) {
-	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	//viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
 	if err != nil {
 		log.Fatalln("cannot read from a config")
