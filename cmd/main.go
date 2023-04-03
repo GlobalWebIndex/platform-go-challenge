1d8a3bf035828e981e69d25bee7c7f2c8ab88886
InnerPeace <dk.optimism.io@gmail.com>
2023-04-03 08:42:24 +0300

    fix cors error

--- cmd/main.go
+++ cmd/main.go
@@ -134,16 +134,24 @@ func main() {
 }
 
 // addCORSHeaders is a middleware function that adds the necessary CORS headers to the HTTP response.
+// addCORSHeaders is a middleware function that adds the necessary CORS headers to the HTTP response.
 func addCORSHeaders(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		setCORSHeaders(w, r)
+		// Preflight request. Reply successfully:
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		handler.ServeHTTP(w, r)
 	})
 }
+
 func setCORSHeaders(w http.ResponseWriter, r *http.Request) {
 	// Add CORS headers to the response
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 }
 
