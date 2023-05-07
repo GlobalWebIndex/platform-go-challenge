3bb2119cd30d4375d5fb4ae5f57859404cbc5470
Loukas Peteinaris <loukas.peteinaris@insurancemarket.gr>
2023-05-07 23:40:52 +0300

    adds documentation

---
+++ docs/swagger.go
@@ -0,0 +1,33 @@
+// Package Docs Platform Go Challenge OpenAPI
+//
+// # Assessment for GWI Platform Go Challange
+//
+// Terms Of Service:
+//
+// there are no TOS at this moment, use at your own risk we take no responsibility
+//
+//	Schemes: http, https
+//	Host: localhost:8080
+//	BasePath: /
+//	Version: 0.0.1
+//	License: MIT http://opensource.org/licenses/MIT
+//	Contact: Loukas Peteinaris<loukas.peteinaris@gmail.com> https://github.com/loukaspe
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//
+// security:
+// - bearer: []
+//
+// securityDefinitions:
+//
+//	bearer:
+//	  type: apiKey
+//	  name: Authorization
+//	  in: header
+//
+// swagger:meta
+package docs
